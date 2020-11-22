43888194ee71f801645890d7f5b0fe70717199bc
Conor Rybacki <conor.rybo@gmail.com>
2020-11-22 17:50:04 -0500

    Update AirportViews.sql

--- AirportViews.sql
+++ AirportViews.sql
@@ -2,7 +2,7 @@
 CREATE VIEW vManagers AS 
 SELECT E.Emp_FName "Employee First Name", E.Emp_LName "Employee Last Name", B.Emp_FName "Boss First Name", B.Emp_LName "Boss Last Name"
 FROM EMPLOYEE E JOIN EMPLOYEE B ON  E.Sup_ID=B.Emp_ID
-ORDER BY B.Emp_FName
+ORDER BY B.Emp_FName;
 
 
 /* Displays all Arriving Flight information and number of passangers on that flight */ 
@@ -11,7 +11,7 @@ select F.Flight_Num 'Flight Number', concat(F.Term_Letter,F.Gate_Num) 'Gate', F.
 from FLIGHT F join PASSENGER P on P.Flight_Num=F.Flight_Num join AIRLINE A on A.Airline_ID=F.Airline_ID 
 group by F.Flight_Num
 having F.Flight_Num < 2000
-concat(F.Term_Letter,F.Gate_Num);
+order by F.Flight_Time;
 
 /* Displays all Departing Flight information and number of passengers on flights */
 create view vDepartingFlights AS
@@ -19,4 +19,4 @@ select F.Flight_Num 'Flight Number', concat(F.Term_Letter,F.Gate_Num) 'Gate', F.
 from FLIGHT F join PASSENGER P on P.Flight_Num=F.Flight_Num join AIRLINE A on A.Airline_ID=F.Airline_ID 
 group by F.Flight_Num
 having F.Flight_Num >= 2000
-order by concat(F.Term_Letter,F.Gate_Num);
+order by F.Flight_Time;
